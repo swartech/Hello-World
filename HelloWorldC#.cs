8b65d745d08b301d500b7f8a22659ae7243f2977
Sam Whelan <swartech@mail.com>
2012-03-10 12:17:18 +0000

    Added HelloWorldC#.cs after eventually getting mono to work

---
+++ HelloWorldC#.cs
@@ -0,0 +1,12 @@
+// A simple C# program to print "Hello World!" to standard output.
+// Written by Sam Whelan
+
+using System;
+ 
+public class HelloWorld
+{
+  public static void Main ()
+  {
+    Console.WriteLine ("Hello World!");
+  }
+}
