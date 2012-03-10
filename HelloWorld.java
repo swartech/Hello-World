77f98e932809944a01b10d75afabfc3da94070b1
Sam Whelan <swartech@mail.com>
2012-03-10 10:50:34 +0000

    Added HelloWorld.java (first program) to repository

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// A spectacularly simple program to print "Hello World!" to standard output.
+// Written by Sam Whelan
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello World!");
+  }
+}
